79184ffd6230cc029ab5bc11aeb8f96327925804
sdmoran <smoran@wpi.edu>
2019-11-11 10:49:02 -0500

    Finishing touches on writeup

--- problem4.m
+++ problem4.m
@@ -22,46 +22,67 @@ figure(4)
 histogram(xy(healthy, 2))
 title('Healthy continuous');
 
-% DISCRETE 
-% Classify diseased individuals
-s = 0;
-for i = 1 : length(has_disease)
-    s = s + (Prob_Disease_Discrete(xy(has_disease(i), 1)));
-end
-
-disp(" DISCRETE ");
-disp("Diseased people categorized as diseased: " + s);
-disp("Total diseased: " + length(has_disease));
-
-% Classify diseased individuals
-s = 0;
-for i = 1 : length(healthy)
-    if(Prob_Disease_Discrete(xy(healthy(i), 1))) == 0
-        s = s + 1;
+% Classify people using discrete basic classifier
+tp = 0;
+tn = 0;
+fp = 0;
+fn = 0;
+for i = 1 : length(xy)
+    % If it's more likely they DON'T have the disease, add 1 to healthy ct
+    result = Prob_Disease_Discrete(xy((i), 1));
+    if result == 1 && xy(i, 3) == 1
+        tp = tp + 1;
+    else
+    if result == 1 && xy(i, 3) == 0
+        fn = fn + 1;
+    else
+    if result == 0 && xy(i, 3) == 1
+        fp = fp + 1;
+    else
+    if result == 0 && xy(i, 3) == 0
+        tn = tn + 1;
+    end
+    end
+    end
     end
 end
-disp("People categorized as healthy: " + s);
-disp("Total healthy: " + length(healthy));
+disp(" DISCRETE");
+disp("TP: " + tp);
+disp("FP: " + fp);
+disp("FN: " + fn);
+disp("TN: " + tn);
 
-% CONTINUOUS
-s = 0;
-for i = 1 : length(has_disease)
-    s = s + (Prob_Disease_Continuous(xy(has_disease(i), 2)));
-end
-
-disp(" CONTINUOUS ");
-disp("Diseased people categorized as diseased: " + s);
-disp("Total diseased: " + length(has_disease));
-
-% Classify diseased individuals
-s = 0;
-for i = 1 : length(healthy)
-    if(Prob_Disease_Continuous(xy(healthy(i), 2))) == 0
-        s = s + 1;
+% Classify people using continuous basic classifier
+tp = 0;
+tn = 0;
+fp = 0;
+fn = 0;
+for i = 1 : length(xy)
+    % If it's more likely they DON'T have the disease, add 1 to healthy ct
+    result = Prob_Disease_Continuous(xy((i), 2));
+    if result == 1 && xy(i, 3) == 1
+        tp = tp + 1;
+    else
+    if result == 1 && xy(i, 3) == 0
+        fn = fn + 1;
+    else
+    if result == 0 && xy(i, 3) == 1
+        fp = fp + 1;
+    else
+    if result == 0 && xy(i, 3) == 0
+        tn = tn + 1;
+    end
+    end
+    end
     end
 end
-disp("People categorized as healthy: " + s);
-disp("Total healthy: " + length(healthy));
+disp(" CONTINUOUS");
+disp("TP: " + tp);
+disp("FP: " + fp);
+disp("FN: " + fn);
+disp("TN: " + tn);
+
+
 
 total_healthy = length(healthy);
 total_diseased = length(has_disease);
@@ -71,12 +92,6 @@ d_disease_yes = find(xy(has_disease, 1) == 1);
 d_disease_no = find(xy(has_disease, 1) == 0);
 d_healthy_yes = find(xy(healthy, 1) == 1);
 d_healthy_no = find(xy(healthy, 1) == 0);
-
-% Positive continuous value -> disease
-c_disease_pos = find(xy(has_disease, 2) > 0);
-c_disease_neg = find(xy(has_disease, 2) <= 0);
-c_healthy_pos = find(xy(healthy, 2) > 0);
-c_healthy_neg = find(xy(healthy, 2) <= 0);
 
 % Probability calculations for discrete case
 % Discrete: disease, no indicator
@@ -90,19 +105,34 @@ disp("P(y='disease'): " + length(has_disease)/ 100);
 disp("P(y='healthy'): " + length(healthy)/ 100);
 
 % Classify people using naive bayes classifier
-healthy_count = 0;
-diseased_count = 0;
+tp = 0;
+tn = 0;
+fp = 0;
+fn = 0;
 for i = 1 : length(xy)
     % If it's more likely they DON'T have the disease, add 1 to healthy ct
-    if Naive_Bayes(xy(i, 1), xy(i, 2)) == 0
-        healthy_count = healthy_count + 1;
+    result = Naive_Bayes(xy(i, 1), xy(i, 2));
+    if result == 1 && xy(i, 3) == 1
+        tp = tp + 1;
     else
-        diseased_count = diseased_count + 1;
+    if result == 1 && xy(i, 3) == 0
+        fn = fn + 1;
+    else
+    if result == 0 && xy(i, 3) == 1
+        fp = fp + 1;
+    else
+    if result == 0 && xy(i, 3) == 0
+        tn = tn + 1;
+    end
+    end
+    end
     end
 end
-disp("Naive Bayes classified healthy: " + healthy_count);
-disp("Naive Bayes classified diseased: " + diseased_count);
-% Discrete is the discrete marker, cont is continuous
+disp(" NAIVE BAYES");
+disp("TP: " + tp);
+disp("FP: " + fp);
+disp("FN: " + fn);
+disp("TN: " + tn);
 
 %!!! NORMAL DIST NEEDS TO HAVE CASES FOR PEOPLE HAVING/NOT HAVING DISEASE
 function disease = Naive_Bayes(disc, cont)
@@ -205,8 +235,14 @@ function disease = Prob_Disease_Continuous(x)
     % Probability that they are HEALTHY given this value of indicator
     healthy_dist = fitdist(xy(healthy, 2), 'Normal');
     
+%     disp("Healthy dist params:");
+%     disp(healthy_dist);
+    
     % Probability that they are DISEASED given this value of indicator
     disease_dist = fitdist(xy(has_disease, 2), 'Normal');
+    
+%     disp("Disease dist params:");
+%     disp(disease_dist);
     
     prob_disease = pdf(disease_dist, x) * length(has_disease) / 100;
     prob_healthy = pdf(healthy_dist, x) * length(healthy) / 100;
